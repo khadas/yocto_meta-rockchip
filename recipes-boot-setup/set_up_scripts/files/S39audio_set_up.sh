032cb778f8325b1dfcdc315700e35b1e82439050
Gray Huang <gray.huang@wesion.com>
2024-05-28 15:33:53 +0800

    Edge2: Audio: restore asound.state for ES8316.
    Signed-off-by: Gray Huang <gray.huang@wesion.com>
    Change-Id: I74794c8e23106882df18558f2502798f47448ac0

--- recipes-boot-setup/set_up_scripts/files/S39audio_set_up.sh
+++ recipes-boot-setup/set_up_scripts/files/S39audio_set_up.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-amixer -c 3 cset numid=28,iface=MIXER,name='ALC Capture Switch' on 1> /dev/null
+CARD_ID=$(aplay -l | grep 8316c | awk '{print $2}' | cut -c 1)
 
-amixer -c 3 cset numid=35,iface=MIXER,name='ALC Capture Noise Gate Switch' 1 1> /dev/null
-
-amixer -c 3 cset numid=15,iface=MIXER,name='DAC Playback Volume' 192 1> /dev/null
-
-amixer -c 3 cset numid=13,iface=MIXER,name='Headphone Mixer Volume' 11 1> /dev/null
-
-amixer -c 3 cset numid=44,iface=MIXER,name='Left Headphone Mixer Left DAC Switch' on 1> /dev/null
-
-amixer -c 3 cset numid=46,iface=MIXER,name='Right Headphone Mixer Right DAC Switch' on 1> /dev/null
-
-amixer -c 3 cset numid=45,iface=MIXER,name='Right Headphone Mixer RLIN Switch' on 1> /dev/null
-
-amixer -c 3 cset numid=18,iface=MIXER,name='DAC Notch Filter Switch' 1 1> /dev/null
+alsactl restore $CARD_ID
